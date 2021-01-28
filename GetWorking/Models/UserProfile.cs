00305ec2b42af1db5512495707b110cb71300257
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-28 13:34:26 -0600

    updated .csproj and appsettings.json, also added userProfile Model

---
+++ GetWorking/Models/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetWorking.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(28)]
+        public string FirebaseUserId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
