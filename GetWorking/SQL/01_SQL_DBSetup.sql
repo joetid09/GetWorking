0ceb59a46d7642d7023da13767988cc779b3d7e1
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-28 13:17:51 -0600

    created sql script and created database

---
+++ GetWorking/SQL/01_SQL_DBSetup.sql
@@ -0,0 +1,60 @@
+﻿USE [master]
+
+IF db_id('GetWorking') IS NULl
+  CREATE DATABASE [GetWorking]
+GO
+
+USE [GetWorking]
+GO
+
+
+DROP TABLE IF EXISTS [Application];
+DROP TABLE IF EXISTS [ApplicationTag];
+DROP TABLE IF EXISTS [Event];
+DROP TABLE IF EXISTS [Tag];
+DROP TABLE IF EXISTS [UserProfile];
+GO
+
+
+CREATE TABLE [Application] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [JobTitle] nvarchar(50) NOT NULL,
+  [Company] nvarchar(50) NOT NULL,
+  [DateApplied] datetime NOT NULL,
+  [UserProfileId] integer NOT NULL
+)
+
+CREATE TABLE [ApplicationTag] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [TagId] Integer NOT NULL,
+  [ApplicationId] Integer NOT NULL
+)
+
+CREATE TABLE [Event] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [Title] nvarchar(50) NOT NULL,
+  [DateCreated] datetime NOT NULL,
+  [DateToComplete] datetime NOT NULL,
+  [Body] nvarchar(256) NOT NULL,
+  [Status] integer NOT NULL,
+  [ApplicationId] integer NOT NULL,
+)
+
+CREATE TABLE [Tag] (
+[Id] integer PRIMARY KEY IDENTITY,
+[Name] nvarchar(25) NOT NULL
+)
+CREATE TABLE [UserProfile] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [FirebaseUserId] NVARCHAR(28) NOT NULL,
+  [FirstName] nvarchar(50) NOT NULL,
+  [LastName] nvarchar(50) NOT NULL,
+  [Email] nvarchar(555) NOT NULL,
+  [CreateDateTime] datetime NOT NULL
+
+  CONSTRAINT UQ_FirebaseUserId UNIQUE(FirebaseUserId),
+  CONSTRAINT UQ_Email UNIQUE(Email)
+)
+
+
+GO
