279449dc6de3639bfc7ffa7a431f37d9e88a49aa
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-29 09:42:12 -0600

    updated sql to alter tables with foreign key constrains

--- GetWorking/Models/Event.cs
+++ GetWorking/Models/Event.cs
@@ -15,7 +15,7 @@ namespace GetWorking.Models
         [Required]
         public DateTime Datecreated { get; set; }
         [Required]
-        public DataType DateToComoplete { get; set; }
+        public DataType DateToComplete { get; set; }
         [Required]
         [MaxLength(256)]
         public string Body { get; set; }
