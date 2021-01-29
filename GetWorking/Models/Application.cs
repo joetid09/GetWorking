32e2d88421af6131784c55ece9170d60a52112db
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-29 09:27:32 -0600

    created models for each needed table in DB

---
+++ GetWorking/Models/Application.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetWorking.Models
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string JobTitle { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Company { get; set; }
+        [Required]
+        public DateTime DateApplied { get; set; }
+        [Required]
+        public int ApplicationId { get; set; }
+    }
+}
---
+++ GetWorking/Models/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetWorking.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        [Required]
+        public DateTime Datecreated { get; set; }
+        [Required]
+        public DataType DateToComoplete { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Body { get; set; }
+        public int Status { get; set; }
+        [Required]
+        public int ApplicationId { get; set; }
+        public Application Application {get; set;}
+    }
+}
