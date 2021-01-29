9d4703f9163f9ffa0ea0f47f7216c72912db1080
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-29 10:49:10 -0600

    add repo/interface/and controller for events, showing in swagger

--- GetWorking/Repositories/EventRepositorycs.cs
+++ GetWorking/Repositories/EventRepositorycs.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using GetWorking.Data;
+using GetWorking.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GetWorking.Repositories
 {
-    public class EventRepositorycs
+    public class EventRepository : IEventRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public EventRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Event> GetByApplicationId(int applicationId)
+        {
+            return _context.Event
+                .Where(ep => ep.Id == applicationId)
+                .ToList();
+
+
+        }
+
+        public void Add(Event newEvent)
+        {
+            _context.Add(newEvent);
+            _context.SaveChanges();
+        }
+
+
+
+
     }
 }
+
