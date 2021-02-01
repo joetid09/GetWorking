75a715f7a159ba3da2eaf87dc21d73a4d0fdaa06
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-31 18:06:56 -0600

    created fields file, export fields for NewApplication Form

--- GetWorking/client/src/components/Form/Fields.js
+++ GetWorking/client/src/components/Form/Fields.js
@@ -1,3 +1,11 @@
 const fields = {
-
+    JobTitle: { type: "text", name: "jobTitle", text: "Job Title", placeholder: "Title", required: true },
+    Company: { type: "text", name: "company", text: "Company", required: true },
+    DateApplied: { type: "date", name: "dateApplied", text: "Date Applied", required: true },
 }
+
+export const ApplicationForm = [
+    fields.JobTitle,
+    fields.Company,
+    fields.DateApplied
+]
