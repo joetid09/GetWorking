8098df800519581b24ae06a0b3afa822e73b4470
joseph tidwell <joseph.tidwell09@gmail.com>
2021-02-02 16:03:11 -0600

    not able to get postman to actuall edit. took out authentication step

--- GetWorking/Controllers/ApplicationController.cs
+++ GetWorking/Controllers/ApplicationController.cs
@@ -53,10 +53,6 @@ namespace GetWorking.Controllers
         public IActionResult UpdateApplication(int id, Application application)
         {
             var OriginalApplication = _appRepo.GetByApplicationId(id);
-            if(id != application.Id)
-            {
-                return BadRequest();
-            }
             OriginalApplication.Company = application.Company;
             OriginalApplication.JobTitle = application.JobTitle;
             OriginalApplication.DateApplied = application.DateApplied;
